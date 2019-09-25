b6d67e10aff2158887b52c44e2a4c26601d316a0
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-09-25 22:29:57 +0300

    Init project

---
+++ client/src/components/features/Posts/Posts.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import PostsList from '../PostsList/PostsList';
+import Spinner from '../../common/Spinner/Spinner';
+import Alert from '../../common/Alert/Alert';
+
+
+class Posts extends React.Component {
+
+  componentDidMount() {
+    const { loadPosts } = this.props;
+    loadPosts();
+  }
+
+  render() {
+    const {posts, request} = this.props;
+
+    const textin =  request.pending ? ( 
+        <Spinner /> 
+      ) : request.success ? ( 
+         posts.length > 0 ? (
+          <PostsList posts={posts} /> 
+      ) : ( 
+        <Alert variant="info"> No posts!!! </Alert>
+      )) : ( 
+        <Alert variant="error"> {request.error} </Alert>
+      );
+
+    return (
+      <div> {textin} </div>
+    );
+  }
+
+};
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ),
+  loadPosts: PropTypes.func.isRequired,
+};
+
+export default Posts;
