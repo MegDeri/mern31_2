674665db2ed65b6e42678dcc921a87f2562d77a8
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-02 21:43:23 +0300

    Modify config.

--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  PORT: 8000,
-  DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
+  PORT: process.env.PORT ? process.env.PORT : 8000,
+  DB: process.env.DB
+    ? process.env.DB
+    : 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority'
 };
