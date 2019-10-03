e3594344cefdd049e0e94a325c255aac2980010d
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-03 22:13:51 +0300

    Modify config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,12 @@
+// module.exports = {
+//   PORT: 8000,
+//   DB: process.env.DB
+//     ? process.env.DB
+//     : 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority'
+// };
+
 module.exports = {
-  PORT: 8000,
-  DB: process.env.DB
-    ? process.env.DB
-    : 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority'
-};
+     PORT: 8000,
+     DB: process.env.DB,
+  };
+  
