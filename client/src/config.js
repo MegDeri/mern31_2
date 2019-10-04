7f2d5553c519f910c56aa35ee34be5aa4c8ed253
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-04 23:00:58 +0300

    Modify config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,9 @@
-export const API_URL = 'api';
-export const BASE_URL = 'https://mern31-6.herokuapp.com/';
+// export const API_URL = 'api';
+// export const BASE_URL = 'https://mern31-6.herokuapp.com/';
+
+export const API_URL = process.env.BASE_URL
+  ? process.env.BASE_URL
+  : 'http://localhost:8000/api';
+export const BASE_URL = process.env.BASE_URL
+  ? process.env.BASE_URL
+  : 'http://localhost:3000/';
--- server/config.js
+++ server/config.js
@@ -1,8 +1,13 @@
-    module.exports = {
-      PORT: 8000,
-      DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
-    }
+   //  module.exports = {
+   //    PORT: 8000,
+   //    DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
+   //  }
       
    
     
-    
+   module.exports = {
+      PORT: process.env.PORT ? process.env.PORT : 8000,
+      DB: process.env.DB
+        ? process.env.DB
+        : 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
+   }
