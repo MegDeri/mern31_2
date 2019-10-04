7c8a88ec1ae9b5ccc3ce48a5a58ada06c71489dc
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-04 21:53:20 +0300

    Modify config

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -101,7 +101,7 @@ export const loadPostsRequest = () => {
     };
   };
 
-  export const loadPostsByPageRequest = (page, postsPerPage = 5) => {
+  export const loadPostsByPageRequest = (page, postsPerPage) => {
     return async dispatch => {
   
       dispatch(startRequest());
@@ -112,7 +112,7 @@ export const loadPostsRequest = () => {
         const startAt = (page - 1) * postsPerPage;
         const limit = postsPerPage;
   
-        let res = await axios.get(`${API_URL}/posts/range/${startAt}/${limit}`);
+        let res = await axios.get(`${BASE_URL}/posts/range/${startAt}/${limit}`);
         //await new Promise((resolve, reject) => setTimeout(resolve, 2000));
   
         const payload = {
