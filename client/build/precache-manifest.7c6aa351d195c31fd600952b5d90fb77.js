3a2a64ce4c4dcbd20e1d7eb2e21a95e374446c15
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-07 11:00:54 +0300

    Modify .gitignore

---
+++ client/build/precache-manifest.7c6aa351d195c31fd600952b5d90fb77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010f3badbbd16184271aa008b09eedca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f179d5d167c9db7a9d65",
+    "url": "/static/css/2.b0238cd8.chunk.css"
+  },
+  {
+    "revision": "8b2399551c4e4c07e8bb",
+    "url": "/static/css/main.21532a24.chunk.css"
+  },
+  {
+    "revision": "f179d5d167c9db7a9d65",
+    "url": "/static/js/2.4b620652.chunk.js"
+  },
+  {
+    "revision": "8b2399551c4e4c07e8bb",
+    "url": "/static/js/main.da92c41b.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
