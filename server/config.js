db1472a208ae870da6ae83abad7587d7a62829d0
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-08 19:22:16 +0300

    Set up env_variables

--- server/config.js
+++ server/config.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-      PORT: process.env.PORT || 8000,
-      DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
+      PORT: process.env.PORT,
+      DB: process.env.DB
+      //DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
    }
