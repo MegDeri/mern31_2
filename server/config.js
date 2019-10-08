9a46afa449ff89d5847399a1eb2c7d0ae5325364
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-10-08 19:48:33 +0300

    Modify config

--- server/config.js
+++ server/config.js
@@ -4,5 +4,4 @@ dotenv.config();
 module.exports = {
       PORT: process.env.PORT,
       DB: process.env.DB
-      //DB: 'mongodb+srv://MegDeri:thbjSE8xIAxmNrO5@cluster0-ej9s1.mongodb.net/test?retryWrites=true&w=majority',
-   }
+}
